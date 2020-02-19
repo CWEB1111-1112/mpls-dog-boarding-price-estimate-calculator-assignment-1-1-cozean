7b65e4ee892ad35f8a43ef0e85d915f4025137ef
cifulton <cfulton@dunwoody.edu>
2020-02-19 11:31:49 -0600

    adding starter code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assignment_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
